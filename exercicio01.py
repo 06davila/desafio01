ebfbac703b900d1268e02aed957128c99d8ee09f
davila <davilaketilly59@gmail.com>
2025-04-03 11:54:45 -0300

    exercicio 01

---
+++ exercicio01.py
@@ -0,0 +1,13 @@
+hora1 = int(input("digite a hora: ")
+min1 = int(input("digite o minuto: ")
+hora2=int(f"digite a {hora1} e o {minuto1} ")
+min2=int(f"digite a {hora1})
+somaminuto = minuto1+minuto2
+
+  if somamin >=60
+        hora +=1
+  if 
+
+
+
+
